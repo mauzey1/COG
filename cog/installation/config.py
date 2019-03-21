2a9f3acfd35a72303cb66735f0bc7caedcd3a1a0
mauzey1 <mauzey1@llnl.gov>
2019-03-21 15:51:19 -0700

    Make value a string for ConfigParser

--- cog/installation/config.py
+++ cog/installation/config.py
@@ -160,7 +160,7 @@ class CogConfig(object):
         # default search service URL, before any project customization
         self._safeSet('DEFAULT_SEARCH_URL','http://%s/esg-search/search/' % hostName)
         # interval between updates of user's projects, during user session
-        self._safeSet('MY_PROJECTS_REFRESH_SECONDS', 3600)
+        self._safeSet('MY_PROJECTS_REFRESH_SECONDS', '3600')
         # optional number of days after which password expire
         self._safeSet('PWD_EXPIRATION_DAYS','0')
         # optional top-level URL to redirect user registration (no trailing '/')
